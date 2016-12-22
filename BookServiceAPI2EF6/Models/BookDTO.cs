8095aa1395f8e63b3269cb87e93ffcbbb234d4b5
Ali <Ali@DESKTOP-AK>
2016-12-22 11:13:54 +0300

    Book Data Transfer Objects (DTOs)

---
+++ BookServiceAPI2EF6/Models/BookDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookServiceAPI2EF6.Models
+{
+    public class BookDTO//data transfer object
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string AuthorName { get; set; }
+    }
+}
--- BookServiceAPI2EF6/Models/BookServiceAPI2EF6Context.cs
+++ BookServiceAPI2EF6/Models/BookServiceAPI2EF6Context.cs
@@ -14,13 +14,16 @@ namespace BookServiceAPI2EF6.Models
         // automatically whenever you change your model schema, please use data migrations.
         // For more information refer to the documentation:
         // http://msdn.microsoft.com/en-us/data/jj591621.aspx
-    
+
         public BookServiceAPI2EF6Context() : base("name=BookServiceAPI2EF6Context")
         {
+            this.Database.Log=s=>System.Diagnostics.Debug.WriteLine(s);
         }
 
         public System.Data.Entity.DbSet<BookServiceAPI2EF6.Models.Author> Authors { get; set; }
 
         public System.Data.Entity.DbSet<BookServiceAPI2EF6.Models.Book> Books { get; set; }
+
+        
     }
 }
